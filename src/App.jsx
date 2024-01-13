f3a2157b5214f7b0dd81add8dd5dfc22e2ec3fed
xuanchen <98302650+yaslydia@users.noreply.github.com>
2024-01-12 20:00:44 -0800

    use context

--- src/App.jsx
+++ src/App.jsx
@@ -67,8 +67,13 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+  };
+
   return (
-    <CartContext.Provider>
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
